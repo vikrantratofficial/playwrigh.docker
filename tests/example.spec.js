b09a06cf5850f9b46e601af97837af5058439e87
vikrantratofficial <vikrantratofficial>
2026-09-10 17:15:54 +0530

    second comment

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,12 +8,12 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
