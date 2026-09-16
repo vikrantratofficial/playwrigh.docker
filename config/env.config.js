d260c750757a6fbedcc9fc9ecd74744a1c6cbc0f
vikrantratofficial <vikrantratofficial>
2026-09-16 14:42:18 +0530

    add ddt users env

--- config/env.config.js
+++ config/env.config.js
@@ -26,6 +26,32 @@ const config = {
   credentials: {
     email: process.env.LOGIN_EMAIL || '',
     password: process.env.LOGIN_PASSWORD || '',
+    byRole: {
+      SUPER_ADMIN: {
+        email: process.env.SUPER_ADMIN_EMAIL || '',
+        password: process.env.SUPER_ADMIN_PASSWORD || '',
+      },
+      ADMIN: {
+        email: process.env.ADMIN_EMAIL || '',
+        password: process.env.ADMIN_PASSWORD || '',
+      },
+      CUSTOMER: {
+        email: process.env.CUSTOMER_EMAIL || '',
+        password: process.env.CUSTOMER_PASSWORD || '',
+      },
+      SUPPLIER: {
+        email: process.env.SUPPLIER_EMAIL || '',
+        password: process.env.SUPPLIER_PASSWORD || '',
+      },
+      LOGISTIC: {
+        email: process.env.LOGISTIC_EMAIL || '',
+        password: process.env.LOGISTIC_PASSWORD || '',
+      },
+      TRANSPORTER: {
+        email: process.env.TRANSPORTER_EMAIL || '',
+        password: process.env.TRANSPORTER_PASSWORD || '',
+      },
+    },
   },
   apiBasicAuth: {
     user: required('API_BASIC_AUTH_USER'),
